6bbbe8c2805d98abcae8827baebaa779e45ebcfd
mqw07 <Samuelwang0110@gmail.com>
2026-04-24 18:26:02 -0400

    Linked Roboflow API to 'Identify.py', inferenceing roughly works fine. Todo: - Link 'Identify.py' to 'computer_vision.py' - Create new Elixir counting function (Present cards class?)

--- card_data.py
+++ card_data.py
@@ -3,8 +3,13 @@ from pathlib import Path
 
 _ASSETS = Path(__file__).resolve().parent / "Assets"
 CARDS_CSV = _ASSETS / "clash_royale_cards_1.json"
+SAMPLE_JSON = _ASSETS / "sample.json"
 
-def load_cards_json(path: Path) -> list[dict[str, str]]:
+def load_cards_json(path: Path) -> dict:
     with path.open(newline="", encoding="utf-8") as f:
-        return json.load(f)
+        items = json.load(f)["items"]
+        return {card["name"]: card for card in items}
+
+cards = load_cards_json(SAMPLE_JSON)
+print(cards)
 
--- computer_vision.py
+++ computer_vision.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 import numpy as np
+from mss import mss    
 
-needle = cv.imread("Assets/Game_Data/Skeletons_icon.png", cv.IMREAD_GRAYSCALE)
-haystack = cv.imread("Assets/Game_Data/IMG_7789.png", cv.IMREAD_GRAYSCALE)
+capture = {
+            "top": 55,
+            "left": 1755,
+            "width": 760,
+            "height": 1310
+            }
 
-needle = cv.Canny(needle, 50, 150)
-haystack = cv.Canny(haystack, 50, 150)
+def grab_frame(region):
+    with mss() as sct:
+        ss = sct.grab(region)
+        img = np.asarray(ss)
+    return cv.cvtColor(img, cv.COLOR_BGRA2BGR)
 
-result = cv.matchTemplate(haystack, needle, cv.TM_CCORR_NORMED)
-
-cv.imshow("Result", result)
-cv.waitKey()
-cv.destroyAllWindows()
-
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-print('Best match location:', max_loc)
-print('Best match confidence:', max_val)
+cv.imshow('Screenshot', grab_frame(capture))
+cv.waitKey(0)
