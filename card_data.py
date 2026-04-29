31c7d6a31d98a5f527bd190e8b0c53bc993b686e
mqw07 <Samuelwang0110@gmail.com>
2026-04-29 00:31:43 -0400

    Created translation dict.

--- card_data.py
+++ card_data.py
@@ -10,6 +10,13 @@ def load_cards_json(path: Path) -> dict:
         items = json.load(f)["items"]
         return {card["name"]: card for card in items}
 
+translation = {
+                'archere': 'Archers', 'chevalier': 'Knight', 'gargouille': "Minions", 'geant': 'Giant', 'gobelin': 'Goblin',
+                'gobelin_lances': 'Spear Goblins', 'mini_PEKKA': "Mini P.E.K.K.A", 'mousquetaire': 'Musketeer', 'squelette': 'Skeletons',
+                'valkyrie': 'Valkyrie', 'zappy': 'Zappies', 'bat': 'Bats'
+              }
+
+
 if __name__ == '__main__':
     cards = load_cards_json(SAMPLE_JSON)
     print(cards)
